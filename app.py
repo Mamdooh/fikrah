5f2c246d818d9f24ebe8f6fa323df5703dfc68a5
mamdouh <Mamdooh@users.noreply.github.com>
2025-12-23 23:21:21 +0300

    Test CI/CD pipeline

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello world from Python app!\nAWS test\n"
+    return "Hello world from Python app!\nAWS test\nCI/CD is working!\n"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
